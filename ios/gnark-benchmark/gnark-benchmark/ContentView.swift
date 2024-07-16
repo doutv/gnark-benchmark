912f592cf896c26be7dd19d0b1cf54e57d6afc86
Jason.Huang <20609724+doutv@users.noreply.github.com>
2024-07-16 16:59:17 +0800

    ios: add dir path, allow write file on real devices

--- ios/gnark-benchmark/gnark-benchmark/ContentView.swift
+++ ios/gnark-benchmark/gnark-benchmark/ContentView.swift
@@ -1,5 +1,6 @@
 import SwiftUI
 import Gnark
+import Foundation
 
 struct ContentView: View {
     @State private var isRunning = false
@@ -7,7 +8,10 @@ struct ContentView: View {
     @State private var proveMessage = ""
     @State private var algorithmSelection = "EdDSA"
     @State private var systemSelection = "Groth16"
-    
+    var directory: URL {
+        let documentDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+        return documentDirectory.appendingPathComponent("depositStorage")
+    }
     var body: some View {
         VStack {
             Picker("Select Algorithm", selection: $algorithmSelection) {
@@ -22,42 +26,36 @@ struct ContentView: View {
             }
             .pickerStyle(SegmentedPickerStyle())
             .padding()
-            
             Button("Setup") {
                 isRunning = true
                 let startTime = Date()
-                
                 if algorithmSelection == "ECDSA" && systemSelection == "Groth16" {
-                    EcdsaGroth16Setup("")
+                    EcdsaGroth16Setup(directory.filePath)
                 } else if algorithmSelection == "EdDSA" && systemSelection == "Groth16" {
-                    EddsaGroth16Setup("")
+                    EddsaGroth16Setup(directory.filePath)
                 } else if algorithmSelection == "ECDSA" && systemSelection == "Plonk" {
-                    EcdsaPlonkSetup("")
+                    EcdsaPlonkSetup(directory.filePath) //
                 } else if algorithmSelection == "EdDSA" && systemSelection == "Plonk" {
-                    EddsaPlonkSetup("")
+                    EddsaPlonkSetup(directory.filePath)
                 }
-                
                 let endTime = Date()
                 setupMessage = "Setup Time: \(endTime.timeIntervalSince(startTime)) seconds"
                 isRunning = false
             }
             .disabled(isRunning)
             Text(setupMessage)
-            
             Button("Prove and Verify") {
                 isRunning = true
                 let startTime = Date()
-
                 if algorithmSelection == "ECDSA" && systemSelection == "Groth16" {
-                    EcdsaGroth16ProveAndVerify("")
+                    EcdsaGroth16ProveAndVerify(directory.filePath)
                 } else if algorithmSelection == "EdDSA" && systemSelection == "Groth16" {
-                    EddsaGroth16ProveAndVerify("")
+                    EddsaGroth16ProveAndVerify(directory.filePath)
                 } else if algorithmSelection == "ECDSA" && systemSelection == "Plonk" {
-                    EcdsaPlonkProveAndVerify("")
+                    EcdsaPlonkProveAndVerify(directory.filePath)
                 } else if algorithmSelection == "EdDSA" && systemSelection == "Plonk" {
-                    EddsaPlonkProveAndVerify("")
+                    EddsaPlonkProveAndVerify(directory.filePath)
                 }
-                
                 let endTime = Date()
                 proveMessage = "Prove and Verify Time: \(endTime.timeIntervalSince(startTime)) seconds"
                 isRunning = false
@@ -68,10 +66,14 @@ struct ContentView: View {
         .padding()
     }
 }
-
 // Preview
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+extension URL {
+    var filePath: String {
+        absoluteString.replacingOccurrences(of: "file://", with: "")
     }
 }
